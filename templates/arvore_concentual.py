65f600e03bef3bdff0fa600712626436fa30964e
MarceloPatropi <marcelopatropi@gmail.com>
2025-01-22 23:57:32 -0300

    First results

--- templates/arvore_concentual.py
+++ templates/arvore_concentual.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 from manim import *
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
@@ -7,85 +8,109 @@ from tkinter.filedialog import askopenfilename
 class ArvoreConceitual(MovingCameraScene):
     def construct(self):
         def get_data(file_path):
-            data = pd.read_csv(file_path, sep="|", skiprows=1, usecols=[1, 2, 3, 4])
-
-            # Renomear colunas e limpar espaços extras
-            data.columns = ["id", "text", "shape", "parent"]
-            data = data.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-
-            # Converter tipos de colunas
-            for column in data.columns:
-                try:
-                    # Tentar converter para numérico
-                    data[column] = pd.to_numeric(data[column])
-                    # Verificar se a coluna pode ser convertida para inteiro
-                    if pd.api.types.is_float_dtype(data[column]):
-                        if data[column].dropna().apply(float.is_integer).all():
-                            data[column] = data[column].astype('Int64')
-                except ValueError:
-                    # Se a conversão falhar, manter os valores originais
-                    data[column] = data[column].astype(str)
-
-            # Adicionar coluna para armazenar os nós
-            data["node"] = None
-
-            # Criar os nós
-            for _, node in data.iterrows():
-                if node["shape"] == "circle":
-                    node_shape = Circle().scale(0.5).set_fill(BLUE, opacity=0.5)
-                elif node["shape"] == "rectangle":
-                    node_shape = Square().scale(0.5).set_fill(RED, opacity=0.5)
-                elif node["shape"] == "ellipse":
-                    node_shape = Ellipse(width=2.0, height=1.0).set_fill(GREEN, opacity=0.5)
-                else:
-                    node_shape = Dot()
-                
-                node_text = Text(node["text"]).scale(0.5)
-
-                if pd.isna(node["parent"]):
-                    group = VGroup(node_shape, node_text).arrange(UP)
-                else:
-                    group = VGroup(node_shape, node_text).arrange(DOWN)
-                    # parent_node = data.loc[data["id"] == node["parent"]].iloc[0]
-                    # if parent_node is not None:
-                    #     n_sibilings = len(data.loc[data["parent"] == parent_node["id"]])
-                    #     offset = (RIGHT * (node.name - len(data.loc[data["parent"] == parent_node["id"]]) / 2)) * 3  # Aumentar o espaço horizontal
-                    #     group.next_to(parent_node["node"], DOWN, buff=2).shift(offset)  # Aumentar o espaço vertical
-                    #     data.at[node.name, "node"] = group
-
-                data.at[node.name, "node"] = group
-
+            with open(file_path, 'r') as file:
+                data = json.load(file)
             return data
 
-        def create_tree(data):
-            raiz = data.loc[data["parent"].isna()].iloc[0]
-            raiz["node"].move_to(ORIGIN)
-            self.play(Create(raiz["node"]))
-            create_children(raiz)
+        def create_node(node_data, parent_group=None):
+            if node_data["shape"] == "circle":
+                node_shape = Circle().scale(0.5).set_fill(BLUE, opacity=0.5)
+            elif node_data["shape"] == "rectangle":
+                node_shape = Square().scale(0.5).set_fill(RED, opacity=0.5)
+            elif node_data["shape"] == "ellipse":
+                node_shape = Ellipse(width=2.0, height=1.0).set_fill(GREEN, opacity=0.5)
+            else:
+                node_shape = Dot()
+            
+            node_text = Text(node_data["text"]).scale(0.5)
+            if parent_group is None:
+                group = VGroup(node_shape, node_text).arrange(UP)
+#                group.move_to(ORIGIN)
+            else:
+                group = VGroup(node_shape, node_text).arrange(DOWN)
+#                group = VGroup(node_shape, node_text).arrange(DOWN).next_to(parent_group, DOWN, buff=1)
+            return group
+        
+        def create_tree(data, parent_group=None):
+            node = create_node(data)
+            data["node"] = node
+            if data.get("children"):
+                for child in data["children"]:
+                    child_node = create_tree(child, node)
+            return data
 
-        def create_children(raiz, level=0):        
-            children = data.loc[data["parent"] == raiz["id"]]
-            if not children.empty:
-                level += 1
-                index = 0.5
-                for _, node in children.iterrows():
-                    group = node["node"]
-                    offset = (RIGHT * (index - len(children) / 2)) * 4 # Aumentar o espaço horizontal
-                    group.next_to(raiz["node"], DOWN, buff=1).shift(offset)  # Aumentar o espaço vertical
-                    self.play(Create(group))
-                    index += 1
-                    # Adicionar linha curva entre o nó pai e o nó filho
-                    line = CubicBezier(raiz["node"].get_bottom(), raiz["node"].get_bottom() + DOWN, group.get_top() + UP, group.get_top())
-                    self.play(Create(line))
-                    self.play(self.camera.frame.animate.move_to(group))  # Mover a câmera para mostrar o grupo
-                    create_children(node, level)
-    
+#            self.add(node)
+#            if parent_group is None:
+#                group.move_to(ORIGIN)
+            #     self.play(Create(group))
+#            else:
+#                offset = (RIGHT * (index - len(data.get("children", [])) / 2)) * 2  # Aumentar o espaço horizontal
+#                group.next_to(parent_group, DOWN, buff=1) #.shift(offset)  # Aumentar o espaço vertical
+            #     self.play(Create(group))
+            #     # Adicionar linha curva entre o nó pai e o nó filho
+#                line = CubicBezier(parent_group.get_bottom(), parent_group.get_bottom() + DOWN, group.get_top() + UP, group.get_top())
+#                parent_group.add(line)
+            #     self.play(Create(line))
+            #     self.play(self.camera.frame.animate.move_to(group))  # Mover a câmera para mostrar o grupo
+
+#            children = data.get("children", [])
+#            for i, child in enumerate(children):
+#                create_tree(child, node, level + 1, i)
+                #node.add(create_tree(child, node, level + 1, i))    # Não é a melhor forma de fazer isso. O Dot e Text da raiz contam como dois MOBs
+
+            # if level not in nodes_at_level:
+            #      nodes_at_level[level] = []  # Inicializa a lista para este nível
+            # nodes_at_level[level].append(node)
+            # level_group = VGroup(*nodes_at_level[level]).arrange(buff=1)
+#            self.play(Create(group))
+#            return data
+
+        def position_nodes(tree):
+            def calculate_width(node):
+                if node["children"]:
+                    return sum([calculate_width(child) for child in node["children"]])
+                return 1
+            
+            # width = 0
+            # for child in tree["children"]:
+            #     width += calculate_width(child)
+
+            def position_node(node, x_offset=0):
+                if node["children"]:
+                    x = x_offset -0.5
+                    for child in node["children"]:
+                        w = calculate_width(child)
+                        child["node"].next_to(node["node"], DOWN, buff=1).shift(RIGHT * (x - w / 2) * 2) # Aumentar o espaço horizontal
+                        x += w
+                        position_node(child, x_offset + x)
+
+            position_node(tree)
+
+
+        def play_tree(scene, tree):
+            scene.play(Create(tree["node"]))
+            if tree.get("children"):
+                for child in tree["children"]:
+                    play_tree(scene, child)
+                # Acho um bom local para adicionar as linhas
+#                    scene.wait(0.5)
+
         # Abrir janela do sistema para escolher o arquivo de dados
-        Tk().withdraw()
-        file_path = askopenfilename(title="Selecione o arquivo de dados", filetypes=[("Markdown files", "*.md"), ("All files", "*.*")])
+#        Tk().withdraw()
+#        file_path = askopenfilename(title="Selecione o arquivo de dados", filetypes=[("JSON files", "*.json"), ("All files", "*.*")])
+        file_path = "templates/data/história_da_psicologia.json"
 
         data = get_data(file_path)
-        create_tree(data)
+        tree = create_tree(data)
+        position_nodes(tree)
+#        create_tree(data)
+#        tree.arrange_submobjects(DOWN, buff=1)
+#        tree.apply_to_family(lambda mob: mob.arrange_submobjects(DOWN) self.play(FadeIn(mob), self.camera.frame.animate.move_to(mob)))
+#        self.add(tree)
+#        self.play(Create(self))
+        play_tree(self, tree)
+#        console.log(sorted(nodes_at_level.items()))
+#        console.log(tree)
 
 if __name__ == "__main__":
     # Renderizar a cena
